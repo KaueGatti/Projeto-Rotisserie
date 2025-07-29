859aa7bc7d1891594a95092bf7a8ec810146ad80
KaueGatti <kauegattti@gmail.com>
2025-07-29 13:02:42 -0300

    Removendo dependencia do JFoenix

--- ProjetoRotisserieJavaFX/src/main/java/my/company/projetorotisseriejavafx/PaneProdutoController.java
+++ ProjetoRotisserieJavaFX/src/main/java/my/company/projetorotisseriejavafx/PaneProdutoController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package my.company.projetorotisseriejavafx;
 
 import java.net.URL;
@@ -10,22 +6,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import javafx.scene.layout.Pane;
-/**
- * FXML Controller class
- *
- * @author kaueg
- */
-public class PaneProdutoController implements Initializable {
 
+public class PaneProdutoController implements Initializable {
 
     @FXML
     private Pane paneMarmita1;
-    /**
-     * Initializes the controller class.
-     */
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }
